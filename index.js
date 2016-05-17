31d4e923241537f6591abd323b0bf35e9e3f8d9b
dorian <voegeli.dorian@gmail.com>
2016-05-17 06:45:47 -0400

    different webhook lib

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var WEBHOOK_URL = process.env.MY_SLACK_WEBHOOK_URL;
+Slack = require('node-slackr');
+slack = new Slack(WEBHOOK_URL,{
+  channel: "#pointbot-testing",
+  username: "point-bot",
+  icon_emoji: ":robot_face:"
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +19,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/', function(request, response) {
+  /*response.render({
+    "text": "It's 80 degrees right now."
+  });*/
+  slack.notify("Message"); //without callback
 });
 
 app.listen(app.get('port'), function() {
